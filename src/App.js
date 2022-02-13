843463e92854e9659b287e2094ecfbf0b4dee23b
Diwan111 <69912582+Diwan111@users.noreply.github.com>
2022-02-13 23:17:55 +0530

    Form in ReactJs

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import './App.css';
 
 function App() {
 
-  const [status, setStatus] = useState(true);
+  const [name, setName] = useState(null);
+  const [tnc, setTnc] = useState(false);
+  const [interest, setInterest] = useState(null);
+
+  const getFormData = (event) => {
+    console.log(name , interest , tnc);
+    event.preventDefault(); //Hold Submit and Refresh
+  }
 
   return (
     <div className="App">
-      {
-        status?<h1>Hello World!!!!</h1>:null
-      }
-      {/* <button onClick={()=>setStatus(false)}>Hide</button>
-      <button onClick={()=>setStatus(true)}>Show</button> */}
-      <button onClick={()=>setStatus(!status)}>Toggle</button>
+      <h1>Handle Form in React</h1>
+      <form action="" onSubmit={getFormData}>
+        <input type="text" placeholder='Enter Name....' onChange={(e)=>setName(e.target.value)} /> <br /> <br />
+        <select onChange={(e)=>setInterest(e.target.value)}>
+          <option value="">Select Option</option>
+          <option value="">Marvel</option>
+          <option value="">DC</option>
+        </select> <br /> <br />
+        <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)} /> <span>Accept Terms & Conditions</span> <br /> <br />
+        <button type='submit'> Submit </button>
+      </form>
     </div>
   );
 }
