f1a5b7c3ca5d15907580ce5e925359817c37e5d0
Diwan111 <69912582+Diwan111@users.noreply.github.com>
2022-02-14 16:03:30 +0530

    ComponentDidUpdate

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import {React , Component , useState} from 'react';
-import logo from './logo.svg';
+import { React, Component, useState } from 'react';
 import './App.css';
-import User from './Component/User';
 
-class App extends Component{
+class App extends Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      name : "Anish"
+      name: "Anish",
+      count: 0
     }
     console.log("Constructor");
   }
 
-  componentDidMount(){
-    console.log("Component Did Mount");
+  componentDidUpdate(preProps, preState, snapShot) {
+    console.log("Component Did Update", preState.count, this.state.count);
+    console.log(snapShot);
+    // this.setState({count: this.state.count+1});
+    if (preState.count === this.state.count) {
+      console.log("Data is Same");
+    }
   }
 
-  render(){
-    console.log("Render");
-    return(
+  render() {
+
+    // this.setState({name:"Rohit"});
+    // console.log("Render");
+
+    return (
       <div className='App'>
-        <h1>Hello {this.state.data}</h1>
-        <User name={this.state.name} />
-        <button onClick={()=>this.setState({name:"Rohit"})}>Update Name</button>
+        <h1>Hello {this.state.name}</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={() => this.setState({ count: 1 })} >Update Name</button>
       </div>
     );
   }
